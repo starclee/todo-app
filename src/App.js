e5a19dad7b3aa45713f5cdc4a19fa98db7e8ad10
Sabari <sabarivijay214@gmail.com>
2023-11-08 21:58:13 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+  function buttonClick(e) {
+    e.preventDefault();
+    setTask("");
+    setTodos((oldTodo) => [...oldTodo, task]);
+  }
+
+  return (
+    <div>
+      <header className="header"> Todo APP</header>
+      <div className="content">
+        Add Todo List Here....
+        <p>
+          <form onSubmit={buttonClick}>
+            <input
+              // onKeyDown={(event) => {
+              //   if (event["keyCode"] === 13) {
+              //     buttonClick();
+              //   }
+              // }}
+              type="text"
+              placeholder="Enter your todo list"
+              value={task}
+              onChange={(event) => {
+                setTask(event.target.value);
+              }}
+            ></input>{" "}
+            <button type="submit">Add Todo</button>
+          </form>
+        </p>
+        <ul>
+          {todos.map((i) => {
+            return <li>{i}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
